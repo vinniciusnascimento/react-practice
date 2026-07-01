1adc597a1cdd1d126b522d7d53035f230afa17a0
Vinnicius Nascimento <calangowproducer@gmail.com>
2026-06-30 21:41:54 -0300

    feat: add lucide and "Task" component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from "react"
+import Tasks from "./components/Tasks"
 
 function App(){
-  const [message, setMessage] = useState("Clique no botão")
-  return (
-    <div>
-      <h1 className="text-pink-500">{message}</h1>
-      <button onClick={() => {
-        setMessage("Fui clicado!")
-      }}>Clique aqui</button>
+  let tasks = [
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar react e nodejs",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar programação",
+      description: "Estudar react e nodejs",
+      isCompleted: true,
+    },
+  ]
 
+  function changeIsCompleted(){}
+
+  return (
+    <div className="bg-gray-600 w-screen h-screen">
+      <h1 className="text-white text-2xl text-center font-bold p-5">Gerenciador de Tarefas</h1>
+      <Tasks tasks={tasks} />
     </div>
   )
 }
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,19 @@
+import { ChevronRight } from "lucide-react"
+
+function Tasks(props){
+    return (
+        <div className="bg-slate-200 p-4 mx-[400px] rounded-md space-y-2">
+            {props.tasks.map((task) => {
+                return(
+                    <div key={task.id} className="flex flex-row gap-2">
+                        <h2 className={`bg-slate-500 w-full text-white p-2 rounded-md ${task.isCompleted ? "line-through" : ""}`}>{task.title}</h2>
+
+                        <button className="bg-slate-500 text-white p-2 rounded-md"><ChevronRight /></button>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Tasks
